277c7c9c2ec7d76137ff5430ea31f4057d039284
Blauker <giyuudev.contact@gmail.com>
2022-12-14 23:34:06 +0100

    Battle Pass V4

--- src/app/battle-pass/battle-pass.component.ts
+++ src/app/battle-pass/battle-pass.component.ts
@@ -10,121 +10,125 @@ export class BattlePassComponent implements OnInit {
 
   battlePass = [
     {
-      dates: "First Quests : 16 October - 19 October",
+      dates: "First Quests : 11 December to the 13 December",
       pvpQuests: [
         { quest: "Participate to Caligor 2 time.", number: "1." },
         { quest: "Kill [10] peoples in A4.", number: "2." },
       ],
       pveQuests: [
-        { quest: "Participate to the Instant combat 1 time.", number: "1." },
-        { quest: "Successfully complete the raid [Zenas] x25.", number: "2." },
+        { quest: "Successfully complete the raid [Santa Claus] x30.", number: "1." },
+        { quest: "Participate in the Winter World Boss 1 time.", number: "2." },
       ],
       rewards: [
-        { reward: "- 100x Amulet Box (x4)." },
+        { reward: "- 2x White Walker Box." },
         { reward: "- 500x Giant Lump of Gold." },
-        { reward: "- 1x Random BP Box." },
+        { reward: "- 2x Deluxe Christmas Box." },
+        { reward: "- 1x Winter Box." }
       ]
     },
     {
-      dates: "Second Quests : 20 Otctober - 23 October",
+      dates: "Second Quests : 14 December to the 16 December",
       pvpQuests: [
         { quest: "Participate to the A4 World Boss 1 time.", number: "1." },
         { quest: "Kill [15] peoples in A4.", number: "2." },
       ],
       pveQuests: [
-        { quest: "Participate to the World Boss 1 time.", number: "1." },
-        { quest: "Successfully complete the raid [Fernon] x50.", number: "2." },
+        { quest: "Kill [100] \"Snowman\" in the Winter Farm Map.", number: "1." },
+        { quest: "Participate in the Winter World Boss 1 time.", number: "2." },
       ],
       rewards: [
-        { reward: "- 100x Random SP Box (Get a random sp7/8 from all classes)." },
+        { reward: "- 100x Amulet Box (x4)" },
         { reward: "- 750x Giant Lump of Gold." },
-        { reward: "- 1x Random BP Box." },
+        { reward: "- 1x Winter Box." },
       ]
     },
     {
-      dates: "Third Quests : 24 October - 29 October",
+      dates: "Third Quests : 17 December to the 19 December",
       pvpQuests: [
-        { quest: "Participate to the RBB 1 time.", number: "1." },
-        { quest: "Participate in [2] raids in A4.", number: "2." },
+        { quest: "Participate to the Color Pvp 2 times.", number: "1." },
+        { quest: "Participate in [4] raids in A4.", number: "2." },
         
       ],
       pveQuests: [
-        { quest: "Participate to the A4 World Boss 1 time.", number: "1." },
-        { quest: "Successfully complete the raid [Maru] x50.", number: "2." },
+        { quest: "Successfully complete the raid [Santa Claus] x60.", number: "1." },
+        { quest: "Participate in the Winter World Boss 1 time.", number: "2." },
       ],
       rewards: [
-        { reward: "- 15x Dragon Gem." },
+        { reward: "- 20x Dragon Gem." },
         { reward: "- 1000x Giant Lump of Gold." },
-        { reward: "- 1x Random BP Box." },
+        { reward: "- 5x Eternal Ice." },
+        { reward: "- 1x Winter Box." }
       ]
     },
     {
-      dates: "Fourth Quests : 30 October - 2 November",
+      dates: "Fourth Quests : 20 December to the 22 December",
       pvpQuests: [
         { quest: "Participate to the AOT 1 time.", number: "1." },
         { quest: "Kill [20] peoples in A4.", number: "2." },
       ],
       pveQuests: [
-        { quest: "Participate to the Meteorite Game 1 time.", number: "1." },
-        { quest: "Successfully complete the raid [Belial] x25.", number: "2." },
+        { quest: "Successfully complete the raid [Snowman] x10.", number: "1." },
+        { quest: "Participate in the Winter World Boss 1 time.", number: "2." },
       ],
       rewards: [
-        { reward: "- 100x Perfections." },
+        { reward: "- 120x Perfections." },
         { reward: "- 1250x Giant Lump of Gold." },
-        { reward: "- 1x Random BP Box." },
+        { reward: "- 2x Deluxe Christmas Box." },
+        { reward: "- 1x Winter Box." }
       ]
     },
     {
-      dates: "Fifth Quests : 3 November - 6 November",
+      dates: "Fifth Quests : 23 December to the 25 December",
       pvpQuests: [
-        { quest: "Participate to the Color Pvp 2 time.", number: "1." },
-        { quest: "Participate in [4] raids in A4.", number: "2." },
+        { quest: "Participate to the RBB 1 time.", number: "1." },
+        { quest: "Participate in [2] raids in A4.", number: "2." },
       ],
       pveQuests: [
-        { quest: "Participate to the p8.1 world boss 1 time.", number: "1." },
-        { quest: "Successfully complete the raid [Paimon] x25.", number: "2." },
+        { quest: "Kill [100] \"Docile Snowman\" in the Winter Farm Map.", number: "1." },
+        { quest: "Participate in the Winter World Boss 1 time.", number: "2." },
       ],
       rewards: [
-        { reward: "- 9999x Angel feathers." },
+        { reward: "- 3x White Walker Box." },
         { reward: "- 1500x Giant Lump of Gold." },
-        { reward: "- 1x Random BP Box." },
+        { reward: "- 1x Winter Box." },
       ]
     },
     {
-      dates: "Sixth Quests : 7 November - 10 November",
+      dates: "Sixth Quests : 26 December to the 28 December",
       pvpQuests: [
-        { quest: "Participate to A4 World Boss 2 time.", number: "1." },
+        { quest: "Participate to A4 World Boss 1 time.", number: "1." },
         { quest: "Kill [25] peoples in A4.", number: "2." },
       ],
       pveQuests: [
-        { quest: "Participate to the Bomb Runner 1 time.", number: "1." },
-        { quest: "Successfully complete the raid [Sandstorm] x25.", number: "2." },
+        { quest: "Kill [100] \"Amora\" in the Winter Farm Map.", number: "1." },
+        { quest: "Participate in the Winter World Boss 1 time.", number: "2." },
       ],
       rewards: [
-        { reward: "- 5x Blood red Ruby." },
+        { reward: "- 10x Blood red Ruby." },
         { reward: "- 1750x Giant Lump of Gold." },
-        { reward: "- 1x Random BP Box." },
+        { reward: "- 1x Winter Box." },
       ]
     },
     {
-      dates: "Seventh Quests : 11 November - 16 November",
+      dates: "Seventh Quests (Last) : 29 December to the 2 January",
       pvpQuests: [
         { quest: "Participate to Caligor 4 time.", number: "1." },
         { quest: "Kill [100] peoples in A4.", number: "2." },
-        { quest: "Participate in [6] raids in A4.", number: "3." },
+        { quest: "Participate in [6] raids in A4.", number: "3." }
       ],
       pveQuests: [
-        { quest: "Participate to the Color PVP 2 times.", number: "1." },
-        { quest: "Successfully complete the raid [Ibrahim] x5.", number: "2." },
-        { quest: "Successfully complete the raid [Hardcore] x5.", number: "3." },
+        { quest: "Successfully complete the raid [Santa Claus] x100.", number: "1." },
+        { quest: "Successfully complete the raid [Snowman] x20.", number: "2." },
+        { quest: "Kill [200] \"Snowman\", \"Docile Snowman\" and \"Amora\" in the Winter Farm Map.", number: "3." },
+        { quest: "Participate in the Winter World Boss 1 time.", number: "4." }
       ],
       rewards: [
-        { reward: "- 10x Caligor Winning Side Chest." },
+        { reward: "- 5x White Walker Box." },
         { reward: "- 10x Event Ticket." },
-        { reward: "- 2000x Giant Lump of Gold." },
-        { reward: "- 5x Random BP Box." },
+        { reward: "- 3000x Giant Lump of Gold." },
+        { reward: "- 10x Winter Box." },
         { reward: "- 1x Wings Box." },
-        { reward: "- 1x Old Event Box." },
+        { reward: "- 1x Christmas Box." },
       ]
     },
   ]
